f61f548d3fbe3a19cc6f19f7d8d931b40cfea5a5
Nils Hollenstein <nils.hollenstein@gmail.com>
2024-05-24 13:28:39 +0200

    6.1 extended

--- 6.1/script.js
+++ 6.1/script.js
@@ -1,44 +1,59 @@
+let addresses = [];
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.forms.Adressform;
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const formData = new FormData(form);
+    const address = new FormData(form);
+    addresses.push(address);
+    renderList(addresses);
+  });
+});
 
-    const div = document.createElement("div");
-    const nameh3 = document.createElement("h3");
-    const pAdress = document.createElement("p");
-    const pKnownFrom = document.createElement("p");
-    const a = document.createElement("a");
-    div.classList.add("person");
+function renderList(addresses) {
+  const list = document.createElement("list");
+  const div = document.createElement("div");
+  const nameh3 = document.createElement("h3");
+  const pAdress = document.createElement("p");
+  const pKnownFrom = document.createElement("p");
+  const pAge = document.createElement("p");
+  const a = document.createElement("a");
+  div.classList.add("person");
+  list.replaceChildren;
 
-    let firstname = formData.get("firstname");
-    let lastname = formData.get("lastname");
+  for (const address of addresses) {
+    let firstname = address.get("firstname");
+    let lastname = address.get("lastname");
     let duSie;
-    if (formData.get("duSie")) {
+    if (address.get("duSie")) {
       duSie = "Sie";
     } else {
       duSie = "Du";
     }
     nameh3.innerText = `${firstname.trim()} ${lastname.trim()} (${duSie})`;
 
-    let street = formData.get("street");
-    let plz = formData.get("plz");
-    let location = formData.get("location");
+    let street = address.get("street");
+    let plz = address.get("plz");
+    let location = address.get("location");
     pAdress.innerText = `${street.trim()}\n${plz.trim()} ${location.trim()}`;
 
-    let mail_address = formData.get("email");
+    let mail_address = address.get("email");
     a.href = `mailto:${mail_address.trim()}`;
     a.innerText = mail_address.trim();
 
-    let knwonFrom = formData.get("whereKnow");
+    pAge.innerText = `${address.get("age")} Jahre alt`;
+
+    let knwonFrom = address.get("whereKnow");
 
     pKnownFrom.innerText = `Notitz: ${knwonFrom}`;
 
-    div.appendChild(nameh3);
-    div.appendChild(pAdress);
-    div.appendChild(a);
+    div.append(nameh3);
+    div.append(pAdress);
+    div.append(a);
+    div.append(pAge);
     div.append(pKnownFrom);
-    document.body.appendChild(div);
+
+    list.appendChild(div);
+    document.body.appendChild(list);
     document.body.appendChild(document.createElement("br"));
-  });
-});
+  }
+}
